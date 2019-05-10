24c2183b6d03e8147a014ceac9ed9056afcaff39
Mike Harder <mharder@microsoft.com>
2019-05-10 15:18:48 -0700

    Track requests queued and executed

--- net/BlockingHttpClient/Program.cs
+++ net/BlockingHttpClient/Program.cs
@@ -13,7 +13,8 @@ namespace BlockingHttpClient
         private static HttpClient _httpClient = new HttpClient();
         private static Stopwatch _stopwatch = Stopwatch.StartNew();
 
-        private static long _requests;
+        private static long _requestsQueued;
+        private static long _requestsExecuted;
         private static long _responses;
         private static long _responseLatencyTicks;
 
@@ -91,11 +92,12 @@ namespace BlockingHttpClient
         {
             while (true)
             {
-                if (Interlocked.Read(ref _requests) < requestsPerSecond * _stopwatch.Elapsed.TotalSeconds)
+                if (Interlocked.Read(ref _requestsQueued) < requestsPerSecond * _stopwatch.Elapsed.TotalSeconds)
                 {
-                    Interlocked.Increment(ref _requests);
+                    Interlocked.Increment(ref _requestsQueued);
+                    var start = _stopwatch.ElapsedTicks;
 
-                    ThreadPool.QueueUserWorkItem((state) => ExecuteRequest(uri, syncOverAsync));
+                    ThreadPool.QueueUserWorkItem((state) => ExecuteRequest(uri, syncOverAsync, start));
                 }
                 else
                 {
@@ -104,12 +106,14 @@ namespace BlockingHttpClient
             }
         }
 
-        private static async Task ExecuteRequest(Uri uri, bool syncOverAsync)
+        private static async Task ExecuteRequest(Uri uri, bool syncOverAsync, long startTicks)
         {
-            var start = _stopwatch.ElapsedTicks;
+            Interlocked.Increment(ref _requestsExecuted);
+
             var task = _httpClient.GetAsync(uri);
             if (syncOverAsync)
             {
+                await Task.Yield();
                 task.Wait();
             }
             else
@@ -118,7 +122,7 @@ namespace BlockingHttpClient
             }
             var end = _stopwatch.ElapsedTicks;
 
-            Interlocked.Add(ref _responseLatencyTicks, end - start);
+            Interlocked.Add(ref _responseLatencyTicks, end - startTicks);
             Interlocked.Increment(ref _responses);
 
             task.Result.Dispose();
@@ -126,7 +130,7 @@ namespace BlockingHttpClient
 
         private static async Task WriteResults()
         {
-            var lastRequests = (long)0;
+            var lastRequestsQueued = (long)0;
             var lastResponses = (long)0;
             var lastResponseLatencyTicks = (long)0;
             var lastElapsed = TimeSpan.Zero;
@@ -135,9 +139,9 @@ namespace BlockingHttpClient
             {
                 await Task.Delay(TimeSpan.FromSeconds(1));
 
-                var requests = Interlocked.Read(ref _requests);
-                var currentRequests = requests - lastRequests;
-                lastRequests = requests;
+                var requestsQueued = Interlocked.Read(ref _requestsQueued);
+                var currentRequestsQueued = requestsQueued - lastRequestsQueued;
+                lastRequestsQueued = requestsQueued;
 
                 var responses = Interlocked.Read(ref _responses);
                 var currentResponses = responses - lastResponses;
@@ -151,22 +155,24 @@ namespace BlockingHttpClient
                 var currentElapsed = elapsed - lastElapsed;
                 lastElapsed = elapsed;
 
-                WriteResult(requests, responses, currentRequests, currentResponses, currentResponseLatencyTicks, currentElapsed);
+                WriteResult(requestsQueued, Interlocked.Read(ref _requestsExecuted), responses,
+                    currentRequestsQueued, currentResponses, currentResponseLatencyTicks, currentElapsed);
             }
         }
 
-        private static void WriteResult(long totalRequests, long totalResponses,
-            long currentRequests, long currentResponses, long currentResponseLatencyTicks, TimeSpan currentElapsed)
+        private static void WriteResult(long totalRequestsQueued, long totalRequestsExecuted, long totalResponses,
+            long currentRequestsQueued, long currentResponses, long currentResponseLatencyTicks, TimeSpan currentElapsed)
         {
             var currentResponseLatencyMs = ((double)currentResponseLatencyTicks / Stopwatch.Frequency) * 1000;
             var threads = Process.GetCurrentProcess().Threads.Count;
 
             Console.WriteLine(
                 $"{DateTime.UtcNow.ToString("o")}" +
-                $"\tTot Req\t{totalRequests}" +
+                $"\tTot Req\t{totalRequestsQueued}" +
+                $"\tPen Req\t{totalRequestsQueued - totalRequestsExecuted}" +
+                $"\tOut Req\t{totalRequestsExecuted - totalResponses}" +
                 $"\tTot Rsp\t{totalResponses}" +
-                $"\tOut Req\t{totalRequests - totalResponses}" +
-                $"\tCur Q/S\t{Math.Round(currentRequests / currentElapsed.TotalSeconds)}" +
+                $"\tCur Q/S\t{Math.Round(currentRequestsQueued / currentElapsed.TotalSeconds)}" +
                 $"\tCur R/S\t{Math.Round(currentResponses / currentElapsed.TotalSeconds)}" +
                 $"\tCur Lat\t{Math.Round(currentResponseLatencyMs / currentResponses, 0)}ms" +
                 $"\tThreads\t{threads}"
