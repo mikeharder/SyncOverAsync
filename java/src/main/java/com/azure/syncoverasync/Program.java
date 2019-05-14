390ecf60e244506d26331506b46f4a0106f4300c
Jianghao Lu <jianghaolu@users.noreply.github.com>
2019-05-14 01:11:06 -0700

    Add proper clean up after 15 sec

--- java/src/main/java/com/azure/syncoverasync/Program.java
+++ java/src/main/java/com/azure/syncoverasync/Program.java
@@ -1,26 +1,37 @@
 package com.azure.syncoverasync;
 
 import org.apache.commons.lang3.time.StopWatch;
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
 
 import java.time.Duration;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Program {
 
     private static final StopWatch STOP_WATCH = new StopWatch();
-
+    private static final AtomicBoolean KILLED = new AtomicBoolean(false);
     private static AtomicLong _requests = new AtomicLong(0);
     private static AtomicLong _responses = new AtomicLong(0);
     private static AtomicLong _responseLatencyTicks = new AtomicLong(0);
+    private static int duration = 15; // sec
 
     private static ReactorNettyClient client;
+    private static ExecutorService pool;
+    private static Disposable reporterDisposable;
 
     public static void main(String[] args) throws Exception {
         String uri = null;
@@ -36,58 +47,60 @@ public class Program {
                 requestPerSec = Integer.parseInt(args[i+1]);
             } else if (args[i].equalsIgnoreCase("-soa")) {
                 syncOverAsync = SyncOverAsyncOption.valueOf(args[i+1]);
+            } else if (args[i].equalsIgnoreCase("-duration")) {
+                duration = Integer.parseInt(args[i+1]);
             }
         }
 
-        System.out.println(String.format("Running config: uri: %s, request/sec: %d, syncOverAsync: %s", uri, requestPerSec, syncOverAsync));
+        System.out.println(String.format("Running config: uri: %s, request/sec: %d, syncOverAsync: %s, duration: %d sec", uri, requestPerSec, syncOverAsync, duration));
 
         WriteResults();
 
         client = new ReactorNettyClient(uri);
 
-        ExecutorService pool = null;
-
-        try {
-            if (syncOverAsync == SyncOverAsyncOption.NETTY_SYNC_OVER_ASYNC) {
-                pool = Executors.newCachedThreadPool();
-            }
+        if (syncOverAsync == SyncOverAsyncOption.NETTY_SYNC_OVER_ASYNC) {
+            pool = Executors.newCachedThreadPool();
+        }
 
 //        if (syncOverAsync == SyncOverAsyncOption.NETTY_REACTOR) {
 //            Mono.just(_requests)
 //                    .repeatWhen(fl -> fl.)
 //        }
+        List<Disposable> disposables = new ArrayList<>();
 
-            while (!Thread.currentThread().isInterrupted()) {
-                if (_requests.get() < (requestPerSec * STOP_WATCH.getTime(TimeUnit.MILLISECONDS) / 1000.0)) {
-                    _requests.incrementAndGet();
+        while (!KILLED.get()) {
+            if (_requests.get() < (requestPerSec * STOP_WATCH.getTime(TimeUnit.MILLISECONDS) / 1000.0)) {
+                _requests.incrementAndGet();
 
-                    long start = STOP_WATCH.getTime(TimeUnit.MILLISECONDS);
+                long start = STOP_WATCH.getTime(TimeUnit.MILLISECONDS);
 
-                    if (syncOverAsync == SyncOverAsyncOption.NETTY_ASYNC) {
-                        client.sendAsync().doOnSuccess(s -> {
-                            long end = STOP_WATCH.getTime(TimeUnit.MILLISECONDS);
+                if (syncOverAsync == SyncOverAsyncOption.NETTY_ASYNC) {
+                    disposables.add(client.sendAsync().doOnSuccess(s -> {
+                        long end = STOP_WATCH.getTime(TimeUnit.MILLISECONDS);
+                        _responseLatencyTicks.addAndGet(end - start);
+                        _responses.incrementAndGet();
+                    }).subscribe());
+                } else if (syncOverAsync == SyncOverAsyncOption.NETTY_SYNC_OVER_ASYNC) {
+                    CompletableFuture.supplyAsync(() -> client.send(), pool)
+                            .whenComplete((res, t) -> {
+                                long end = STOP_WATCH.getTime(TimeUnit.MILLISECONDS);
 
-                            _responseLatencyTicks.addAndGet(end - start);
-                            _responses.incrementAndGet();
-                        }).subscribe();
-                    } else if (syncOverAsync == SyncOverAsyncOption.NETTY_SYNC_OVER_ASYNC) {
-                        CompletableFuture.supplyAsync(() -> client.send(), pool)
-                                .whenComplete((res, t) -> {
-                                    long end = STOP_WATCH.getTime(TimeUnit.MILLISECONDS);
-
-                                    _responseLatencyTicks.addAndGet(end - start);
-                                    _responses.incrementAndGet();
-                                });
-                    }
-                } else {
-                    Thread.sleep(1);
+                                _responseLatencyTicks.addAndGet(end - start);
+                                _responses.incrementAndGet();
+                            });
                 }
-            }
-        } finally {
-            if (pool != null) {
-                pool.shutdown();
+            } else {
+                Thread.sleep(1);
             }
         }
+
+        System.out.println("Exiting...");
+        for (Disposable disposable : disposables) {
+            if (!disposable.isDisposed()) {
+                disposable.dispose();
+            }
+        }
+        System.exit(0);
     }
 
     private static void WriteResults() {
@@ -98,8 +111,8 @@ public class Program {
 
         STOP_WATCH.start();
 
-        Flux.interval(Duration.ofSeconds(1))
-                .takeUntil(l -> Thread.currentThread().isInterrupted())
+        reporterDisposable = Flux.interval(Duration.ofSeconds(1))
+                .take(duration)
                 .doOnNext(s -> {
 
                     long requests = _requests.get();
@@ -121,6 +134,13 @@ public class Program {
                     WriteResult(requests, responses, currentRequests, currentResponses, currentResponseLatencyTicks, currentElapsed);
                 })
                 .subscribeOn(Schedulers.newSingle("reporter"))
+                .doOnComplete(() -> {
+                    System.out.println("Reporter exiting...");
+                    KILLED.set(true);
+                    if (pool != null) {
+                        pool.shutdown();
+                    }
+                })
                 .subscribe();
     }
 
