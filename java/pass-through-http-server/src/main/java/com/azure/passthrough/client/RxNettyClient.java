73c18fd505977a27070a8c14f82fed0e2fb72aba
Jianghao Lu <jianghaolu@users.noreply.github.com>
2019-05-20 13:37:40 -0700

    Fix RxNettyClient

--- java/pass-through-http-server/src/main/java/com/azure/passthrough/client/RxNettyClient.java
+++ java/pass-through-http-server/src/main/java/com/azure/passthrough/client/RxNettyClient.java
@@ -1,37 +1,30 @@
 package com.azure.passthrough.client;
 
-import com.microsoft.rest.v2.SwaggerMethodParser;
-import com.microsoft.rest.v2.http.HttpClient;
-import com.microsoft.rest.v2.http.HttpMethod;
-import com.microsoft.rest.v2.http.HttpRequest;
-import com.microsoft.rest.v2.http.NettyClient;
-import com.microsoft.rest.v2.protocol.HttpResponseDecoder;
-import com.microsoft.rest.v2.serializer.JacksonAdapter;
+import com.microsoft.rest.v2.RestProxy;
+import com.microsoft.rest.v2.annotations.GET;
 import io.reactivex.Flowable;
+import io.reactivex.Single;
 import reactor.core.publisher.Mono;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class RxNettyClient implements SyncHttpClient, AsyncHttpClient {
-    private HttpClient client;
+    public interface RxNettyService {
+        @GET("{url}")
+        Single<String> get(String url);
+    }
+
+    private RxNettyService client;
 
     public RxNettyClient() {
-        client = NettyClient.createDefault();
+        client = RestProxy.create(RxNettyService.class)
                 ;
 //                .tcpConfiguration(tcpClient -> tcpClient.proxy(ts -> ts.type(Proxy.HTTP).address(new InetSocketAddress("localhost", 8888))));
     }
 
     public Mono<String> sendAsync(String uri, AtomicLong sent) {
-        try {
-            HttpRequest request = new HttpRequest(null, HttpMethod.GET, new URL(uri), new HttpResponseDecoder(null, new JacksonAdapter()));
-            return Mono.from(client.sendRequestAsync(request).doOnSubscribe(d -> sent.incrementAndGet())
-                    .flatMap(res -> res.bodyAsString())
-                    .flatMapPublisher(Flowable::just));
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
+        return Mono.from(client.get(uri).doOnSubscribe(d -> sent.incrementAndGet())
+                .flatMapPublisher(Flowable::just));
     }
 
     // not recommended
